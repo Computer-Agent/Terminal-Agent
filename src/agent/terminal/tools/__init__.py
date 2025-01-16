2ce5e752f2cef248276ecf8ab802a52d54a2e391
Jeomon <jeogeoalukka@gmail.com>
2025-01-16 11:04:17 +0530

    Included a default mode for the shell in shell tool to use bash or command prompt

--- src/agent/terminal/tools/__init__.py
+++ src/agent/terminal/tools/__init__.py
@@ -3,10 +3,12 @@ from src.tool import Tool
 from subprocess import run
 
 @Tool('Shell Tool',params=Shell)
-def shell_tool(shell: str,command: str) -> str:
+def shell_tool(shell: str=None,command: str='') -> str:
     '''Executes a shell command on the system and returns the output.'''
     try:
-        if shell.lower()=='powershell': #For Windows
+        if shell is None:
+            response=run(command.split(),capture_output=True,text=True)
+        elif shell.lower()=='powershell': #For Windows
             response=run([shell,'-Command']+command.split(),capture_output=True,text=True)
         else: #For Linux,MacOS
             response=run([shell,'-c']+command.split(),capture_output=True,text=True)
