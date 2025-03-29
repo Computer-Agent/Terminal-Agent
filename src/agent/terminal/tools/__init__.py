5ca5355c8b0c37a9155c996931f2fb56a6446591
Jeomon <jeogeoalukka@gmail.com>
2025-03-29 21:10:17 +0530

    python tool has package feature

--- src/agent/terminal/tools/__init__.py
+++ src/agent/terminal/tools/__init__.py
@@ -1,29 +1,31 @@
 from src.agent.terminal.tools.views import Shell,Python
+from typing import Literal
 from src.tool import Tool
-from subprocess import run
+from subprocess import run,check_call
 from platform import system
+from importlib.util import find_spec
 import sys
 import io
 
 os=system()
 
 @Tool('Shell Tool',params=Shell)
-def shell_tool(shell: str=None,command: str='') -> str:
+def shell_tool(shell: str=None,cmd: str='') -> str:
     '''Executes a shell command on the system and returns the output.'''
     elevated_privileges:bool=False
     try:
         if os=='Windows':
             if shell is None:
                 admin_prefix = ["runas","/noprofile","/user:Administrator"] if elevated_privileges else []
-                response=run(admin_prefix+command.split(),capture_output=True,text=True)
+                response=run(admin_prefix+cmd.split(),capture_output=True,text=True)
             elif shell.lower()=='powershell':
                 admin_prefix = [] if elevated_privileges else []
-                response=run(admin_prefix+[shell,'-Command']+command.split(),capture_output=True,text=True)
+                response=run(admin_prefix+[shell,'-Command']+cmd.split(),capture_output=True,text=True)
             else:
                 return 'Shell not supported for Windows'
         elif os=='Linux' or os=='Darwin':
             sudo_prefix = ["sudo"] if elevated_privileges else []
-            response=run(sudo_prefix+command.split(),capture_output=True,text=True)
+            response=run(sudo_prefix+cmd.split(),capture_output=True,text=True)
         else:
             raise Exception('OS not supported')
         if response.returncode==0:
@@ -34,15 +36,28 @@ def shell_tool(shell: str=None,command: str='') -> str:
         return f'Error: {e}'
     
 @Tool('Python Tool',params=Python)
-def python_tool(script: str) -> str:
+def python_tool(mode:Literal['script','package']='script',script: str='',packages: list[str]=[]) -> str:
     '''Executes a python script and returns the output.'''
-    stdout=sys.stdout
-    sys.stdout=io.StringIO()
-    try:
-        exec(script)
-        output=sys.stdout.getvalue()
-    except Exception as e:
-        output=f'Error: {e}'
-    finally:
-        sys.stdout=stdout
-    return output.strip()
+    if mode=='script':
+        stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        try:
+            exec(script)
+            output=sys.stdout.getvalue()
+        except Exception as e:
+            output=f'Error: {e}'
+        finally:
+            sys.stdout=stdout
+        return output.strip()
+    elif mode=='package':
+        packages=list(filter(lambda package: find_spec(package) is None,packages))
+        if not packages:
+            return 'All packages are already installed'
+        packages_string=' '.join(packages)
+        try:
+            check_call([sys.executable, "-m", "pip", "install", packages_string])
+        except Exception as e:
+            return f'Error: {e}'
+        return f'{packages_string} installed successfully'
+    else:
+        raise Exception('Mode not supported')
--- src/agent/terminal/tools/views.py
+++ src/agent/terminal/tools/views.py
@@ -1,9 +1,12 @@
 from pydantic import BaseModel, Field
+from typing import Literal
 
 class Shell(BaseModel):
     shell:str = Field(...,description="Choose the shell according to the OS.",examples=['powershell','bash','cmd'])
-    command: str = Field(..., description="The command to be executed according to the shell.",examples=['pip install flask','python app.py','cd ..','ls -l','Get-ComputerInfo','Get-PSDrive','Get-Process'])
+    cmd: str = Field(..., description="The command to be executed according to the shell.",examples=['python app.py','cd ..','ls -l','Get-ComputerInfo','Get-PSDrive','Get-Process'])
     # elevated_privileges: bool=Field(description="Indicates whether the command requires elevated privileges.",examples=[True,False],default=False)
 
 class Python(BaseModel):
-    script: str = Field(..., description="The python script to be executed.",examples=['''print("Hello World")'''])
+    mode:Literal['script','package']=Field(description="The mode of operation.",examples=['script','package'],default='script')
+    script: str = Field(description="The python script to be executed.",examples=['''print("Hello World")'''],default='')
+    packages: list[str] = Field(description="The python package to be installed.",examples=['flask<=2.2','requests'],default=[])
